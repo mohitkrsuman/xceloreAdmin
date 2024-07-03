28d189de349c26cc14aaaf53a59232d8a3aa6dda
Mohit Kumar Suman <m123aryan123@gmail.com>
2024-07-03 19:41:30 +0530

    updated vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
      'proxy': {
-        '/api': 'https://xceloreadmin.onrender.com/',
+        '/api': 'https://xceloreadmin.onrender.com',
      }
   }
 })
